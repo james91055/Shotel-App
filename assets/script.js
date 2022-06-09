0d82eecd92f16f35c3fa8370feb836f05450aa3b
onionchop <ruthhyo@gmail.com>
2022-06-09 15:30:35 -0400

    fetches

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,17 @@
-fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=Oklahoma City&localStartDateTime=2022-10-07T14:00:00')
+fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=&localStartDateTime=')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
   });
+
+//   fetch('')
+
+//   _embedded.events[_embedded 22].venues.postalCode.val()
+// function fetchData('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city input '){
+//     const response = await fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=&localStartDateTime=');
+//     const data = await response.json();
+
+//     return data;
+// }
