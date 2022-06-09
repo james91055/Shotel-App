f15e41940a620275a287536be779d900a380be07
Jordan Schneringer <schneringerj@gmail.com>
2022-06-09 12:26:15 -0700

    pushing ticketmaster to main

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=Oklahoma City&localStartDateTime=2022-10-07T14:00:00')
+fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=OklahomaCity&localStartDateTime=2022-10-07T14:00:00')
   .then(function (response) {
     return response.json();
   })
