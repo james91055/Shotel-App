1034ad8b4a7aa2c6930d16cd5157cfedbd11499d
James <james@MacBook-Air.local>
2022-06-09 17:16:40 -0700

    update city input, datepicker input, search button

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,31 @@
-console.log(date);
+var dateEl = $("#datepicker");
+var cityEl = $("#city-input");
+var searchBtn = $("#search-button");
 
-fetch(
-  "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=OklahomaCity&localStartDateTime=2022-10-07T14:00:00"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-
-var date = $("#datepicker").val();
 $(function () {
   $("#datepicker").datepicker();
+  console.log(dateEl);
+});
+//Search Button listn to click event
+searchBtn.click(function () {
+  //change date to the format match ticket master
+  var date = moment(dateEl.val()).format("YYYY-MM-DD");
+  var city = cityEl.val();
   console.log(date);
+  console.log(city);
+
+  //fetch data with city variable and date variable
+  fetch(
+    "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=93pGEEPMzqYFAEUn3cg6mvaHS5XBAZPt&city=" +
+      city +
+      "&startDateTime=" +
+      date +
+      "T00:00:00Z"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 });
