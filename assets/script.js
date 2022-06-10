0f9d4c13c0e3979cb0838ec377574b74f9941253
James <james@macbook-air.lan>
2022-06-10 14:51:42 -0700

    add type dropdown, and input to Yelp Api

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,18 @@ var dateEl = $("#datepicker");
 var dateEndEl = $("#datepicker-end");
 var cityEl = $("#city-input");
 var searchBtn = $("#search-button");
-var eventsList = document.querySelector("ul");
+var eventsList = document.querySelector(".event-list");
+var eventZip;
+var li;
+var typeDropdownEl = $("#restaurant-dropdown");
+var restaurantSearchBtn = $("#restaurant-search-button");
 
 $(function () {
   $("#datepicker").datepicker();
   $("#datepicker-end").datepicker();
+});
+$(function () {
+  $("#restaurant-dropdown").selectmenu();
 });
 
 //Search Button listn to click event
@@ -35,8 +42,9 @@ searchBtn.click(function () {
     })
     .then(function (data) {
       console.log(data);
-      console.log(data._embedded.events);
       console.log(data._embedded.events[0]._embedded.venues[0].postalCode);
+      eventZip = data._embedded.events[0]._embedded.venues[0].postalCode;
+      console.log(eventZip);
       console.log(data._embedded.events[0].dates.start.dateTime);
       console.log(data._embedded.events[0].dates.start.localDate);
       for (var i = 0; i < data._embedded.events.length; i++) {
@@ -48,18 +56,27 @@ searchBtn.click(function () {
 });
 
 //yelp api
-fetch(
-  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=92618&size=9",
-  {
-    headers: {
-      Authorization:
-        "Bearer N6Skfd21VaE0zmjYVT-rOZVHwzPzmZj4QHBmzZs27iK8Yctlg_UGniBPqkk5VBA5Tb45MsoTiTR2YxypBzuvbyQ-axQ_nU4V6Q8b07tcDK2iDaADLFpPjVejhG2iYnYx",
-    },
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+
+restaurantSearchBtn.click(function () {
+  var restaurantType = typeDropdownEl.val().toLowerCase();
+
+  fetch(
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" +
+      eventZip +
+      "&size=9&categories=" +
+      restaurantType,
+    {
+      headers: {
+        Authorization:
+          "Bearer N6Skfd21VaE0zmjYVT-rOZVHwzPzmZj4QHBmzZs27iK8Yctlg_UGniBPqkk5VBA5Tb45MsoTiTR2YxypBzuvbyQ-axQ_nU4V6Q8b07tcDK2iDaADLFpPjVejhG2iYnYx",
+      },
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  console.log(restaurantType);
+});
